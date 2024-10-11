5f1a155ade0b66fcff0a2829cbb6aee06bd38806
FaurregobFreddy <faurregob@gmail.com>
2024-10-11 00:47:51 -0500

    Actualizacion del Curso

--- Clases_Youtube/src/Logica/Alumno.java
+++ Clases_Youtube/src/Logica/Alumno.java
@@ -5,6 +5,10 @@ public class Alumno {
     String nombre;
     String apellido;
 
+    public Alumno() {
+    
+    }
+    
     public Alumno(int id, String nombre, String apellido) {
         this.id = id;
         this.nombre = nombre;
