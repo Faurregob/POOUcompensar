198762628dd6c6026d4f47a7542970a823d7f55f
FaurregobFreddy <faurregob@gmail.com>
2024-10-11 00:47:05 -0500

    Se creo una carpeta nueva con nuevos cursos

---
+++ Clases_Youtube/src/Logica/POO.java
@@ -0,0 +1,9 @@
+package Logica;
+public class POO {
+    public static void main(String[] args) {
+        Alumno alu1 = new Alumno();
+        Alumno alu2 = new Alumno(5, "Freddy", "Urrego");
+        
+        
+    }
+}
