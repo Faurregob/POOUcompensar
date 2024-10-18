114510e13a99dbad6eab930d5914f35d99f75b55
FaurregobFreddy <faurregob@gmail.com>
2024-10-17 19:04:53 -0500

    Actualizacion  de la clase de POO

---
+++ POO/src/excepcion1/Excepciones1.java
@@ -0,0 +1,37 @@
+package excepcion1;
+
+import java.util.Scanner;
+
+public class Excepciones1 {
+    public static void main(String[] args) {
+        Scanner entradaTeclado = new Scanner(System.in);
+        System.out.println("Ingrese el valor del dividendo");
+        double dividendo, divisor;
+        
+            
+        if(entradaTeclado.hasNextDouble()){
+            dividendo = entradaTeclado.nextDouble();
+        }else{
+            System.out.println("El valor del dividendo no es double");
+        }
+        
+                     
+//        System.out.println("Ingrese el valor del divisor");
+//        if (entradaTeclado.hasNextDouble()) {
+//            divisor = entradaTeclado.nextDouble();
+//        }else{
+//            System.out.println("El valor del divisor no es double");
+//        }
+        
+        
+        
+//        if (divisor!=0) {
+//            double cociente = dividendo/divisor;
+//            System.out.println("El resultado de la division es: " + cociente);
+//        }else{
+//            System.out.println("Estas ingresando el divisor con valor 0.");
+//        }
+//        
+//        System.exit(0);
+    }
+}
